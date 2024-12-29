809e9d4940c6b8da824324afec708069a15df4d3
Mayur <mayurtyagi@unolo.com>
2024-12-29 16:40:36 +0530

    Initial Commit

---
+++ DeepLinkingURLSchemeDemo/DeepLinkingURLSchemeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepLinkingURLSchemeDemoApp.swift
+//  DeepLinkingURLSchemeDemo
+//
+//  Created by Mayur Kant Tyagi on 29/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DeepLinkingURLSchemeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
